eb49e618e219ff178dfd4a75c9aeddd48270e924
naillim <naillim.domingues@alunoescolassociais.org.br>
2024-04-25 08:02:21 -0300

    adiciona js

--- login.js
+++ login.js
@@ -1,14 +1,27 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmpassword').value;
 
-    
     if (!username || !password || !confirmPassword) {
         alert('Por favor, preencha todos os campos.');
         event.preventDefault();
-    } else if (password !== confirmPassword) {
-        alert('As senhas não coincidem.');
-        event.preventDefault();
-    }
+
+
+}
 });
+
+
+// Verifica senhas
+function verificaSenha(){
+    let pswd        = document.getElementById("password").value;
+    let pswdconf    = document.getElementById("confirmPassword").value;
+
+    if (pswd == pswdconf) {
+        alert("As senhas foram digitadas corretamente");
+    }else{
+        alert("Senhas não conferem");
+    }
+}
+
 
